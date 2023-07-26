f5b9bb5df498f53ea1146c0bbc4d85631ef64ef2
Imron Athoriq Firjatulloh <imronarth@gmail.com>
2023-07-26 07:38:43 +0700

    Finished Project

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+    public function create(Request $request){
+        $product = new Product();
+
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->category = $request->category;
+
+        $product->save();
+        return response()->
+        json("Product Successfully Created!");
+    }
+    public function update(Request $request, $id){
+        $product = Product::find($id);
+
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->category = $request->category;
+
+        $product->save();
+        return response()->json($product);
+    }
+
+    public function delete($id){
+        $product = Product::find($id);
+        $product->delete();
+
+        return response()->json('Product Succesfuly deleted!');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+$router->get('/product', 'ProductController@index');
+$router->get('/product/{id}', 'ProductController@show');
+$router->post('/product', 'ProductController@create');
+$router->put('/product/{id}', 'ProductController@update');
+$router->delete('/product/{id}', 'ProductController@delete');
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
